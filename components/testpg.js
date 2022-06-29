cc988e588b75368a326ad286ee40e11133092899
juanCortezUba <juan.diazcortez@fadu.uba.ar>
2022-06-29 18:28:06 -0300

    newuser

--- components/testpg.js
+++ components/testpg.js
@@ -1,29 +1,38 @@
 const { Client } = require("pg");
-
+const config = {
+  host: "192.168.0.120",
+  port: 5432,
+  user: "postgres",
+  password: "postgres",
+  database: "test",
+};
 const newClient = () => {
-  const client = new Client({
+  return new Client({
     host: "192.168.0.120",
     port: 5432,
     user: "postgres",
     password: "postgres",
     database: "test",
   });
-  return client;
 };
 
-const testPg = async (callback) => {
+// Seguridad
+
+const testPg = async (callback, user) => {
   const client = newClient();
   try {
-    console.log("1");
+    /* todo ok  */
     await client.connect();
-    console.log("11");
-    const res = await client.query("SELECT name from test;");
-    console.log("111");
+    qry = {
+      text: "SELECT *  from test where name=$1",
+      values: [user],
+    };
+    const res = await client.query(qry);
     client.end();
-    console.log(res.rows[0].name); // Hello world!
-    console.log("1111");
+    console.log(res);
     callback(null, res.rows);
   } catch (error) {
+    /* ups hubo error */
     client.end();
     console.log("catch error");
     console.log(error);
@@ -49,12 +58,58 @@ const addName = async (callback, name) => {
   }
 };
 
+async function pg_newUser(user, callback) {
+  console.log(user);
+
+  const qry = {
+    text: 'insert into "user" (name,passwd,email,date, status, hash) values($1,$2,$3,$4,$5,md5($2::character varying)::text)',
+    values: [
+      user.name,
+      user.passwd,
+      user.email,
+      user.date,
+      user.status,
+      // "rgdsrtyreytrhffh453",
+    ],
+  };
+
+  const client = newClient();
+  console.log(client);
+  await client.connect();
+
+  client
+    .query(qry)
+    .then((result) => {
+      console.log(result);
+      callback(null, result);
+    })
+    .catch((err) => {
+      console.log(err);
+      callback(err, null);
+    });
+}
+const pg_getUsers = async (callback) => {
+  const client = newClient();
+  await client.connect();
+
+  client
+    .query('select * from "user"; ')
+    .then((result) => {
+      console.log(result);
+      callback(null, result);
+    })
+    .catch((err) => {
+      console.log(err);
+      callback(err, null);
+    });
+};
 function pepe() {
   const client = new Client(config);
+  client.connect();
+
   client
-    .connect()
     .then((r) => r.json())
     .then((j) => console.log(j))
     .catch((error) => console.error(error.message));
 }
-module.exports = { testPg, addName };
+module.exports = { testPg, addName, pg_newUser, pg_getUsers };
--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const PORT = 5000;
+const PORT = 5000; // port
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
 
-const app = express();
-const { testPg, addName } = require("./components/testpg");
-app.use(cors());
+const app = express(); //creo un server express un app
+const {
+  testPg,
+  addName,
+  pg_newUser,
+  pg_getUsers,
+} = require("./components/testpg");
+app.use(cors()); // use cors
 
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.raw());
@@ -27,13 +32,20 @@ app.use(
 
 app.use(express.static(path.join(__dirname, "public")));
 
-// GET POST PUT DELETE
-app.get("/api/start", function (req, res) {
-  let { query, params } = req;
-  let { codigo, user, hash } = query;
-  console.log(query);
+// GET POST PUT DELETE..... (http) CRM CRUD ABM
+// apache file server
+// donde -> url  http://www.ciencia.org/ratones?edad>90&accion=diseccion -> get
+// que  -> api rest
 
-  console.log(params);
+// www.ciencia.org   es un domain y nesesita resolverse.
+//  necesito un ip entonces www.ciencia.org lo atrapa un dns
+// domain name service  devuelve ip
+//
+app.get("/api/start", function (req, res) {
+  let { query } = req; //
+  let { codigo, user, hash } = query;
+
+  console.log(query);
 
   res.status(200).json({
     message: `Bienvenido usuario ${user} con código ${codigo}`,
@@ -54,16 +66,34 @@ app.post("/api/user/setcreds", (req, resp) => {
   }
 });
 
-app.post("/query", (req, resp) => {
-  console.log("query");
-  testPg((err, result) => {
-    if (err) {
-      console.log("error");
-      resp.status(512).json({ errorMessage: err });
+app.post("/getusers", (req, resp) => {
+  console.log("getusers");
+  pg_getUsers((error, result) => {
+    if (error) {
+      return resp.status(215).json({ error });
     }
-    console.log("ok");
-    resp.status(200).json({ data: result });
+    return resp.status(200).json({ result });
   });
+});
+app.post("/newuser", (req, resp) => {
+  console.log("/newuser");
+  const { user } = req.body;
+  pg_newUser(user, (error, result) => {
+    if (error) {
+      return resp.status(215).json({ error });
+    }
+    return resp.status(200).json({ data: result.rows });
+  });
+});
+
+app.post("/query", (req, resp) => {
+  console.log(`query ${JSON.stringify(req.body)}`);
+  const { user } = req.body;
+
+  testPg((err, result) => {
+    if (err) resp.status(512).json({ error: err });
+    resp.status(200).json({ data: result });
+  }, user);
 });
 
 app.get("/addName", (req, resp) => {
